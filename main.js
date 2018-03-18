9dff719e70cabde17a8d4fb9d18b08322d6b4fa3
Moulindra Muchumari <moulindra1729@gmail.com>
2018-03-18 18:29:40 -0400

    added custom maps, styles and drawings

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
- 
+let mapboxgl = require('mapbox-gl');
+let MapboxDraw = require('@mapbox/mapbox-gl-draw');
+let path = require('path');
+
+const mapsFolder = path.resolve(__dirname, 'maps/');
+const mapsFile = 'map.json';
+var stl_json = require('./styles/json-style.json');
+
+
+// READ maps file
+var geojsonData = require(path.resolve(mapsFolder, mapsFile));
+
+// CREATE custom map style
+delete stl_json.sources['mapbox'];
+stl_json.sources['jsonsource'] = {
+    type: 'geojson',
+    data: geojsonData
+};
+
+// CREATE map object
 mapboxgl.accessToken = 'pk.eyJ1IjoibW0xNzI5IiwiYSI6ImNqZWJ2dGY5aTBqZTEycWxhejBxc2hldWQifQ.EMkZrtcfV-TRMg04cBInvg';
+
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-streets-v10',
-    center: [-74.50, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    container: 'map', // container id
+    style: stl_json,
+    // style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.466, 40.5463], // starting position [lng, lat]
+    zoom: 14, // starting zoom
+    pitch: 45,
+    bearing: -17.6,
+    hash: true
 });
+
+// ENABLE map draw controls
+var draw = new MapboxDraw({
+    displayControlsDefault: true,
+    controls: {
+        point: true,
+        polygon: true,
+        trash: true
+    }
+});
+
+map.addControl(draw);
